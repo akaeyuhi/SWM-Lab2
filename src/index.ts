b4fcf536d7509468667748b5226c65e8859f7f98
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-28 18:56:49 +0300

    LinkedList class constructor and get tails/head method

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,21 @@ interface Node<Type> {
 }
 
 
-export default class LinkedList<Type> {}
+export default class LinkedList<Type> {
+  private head: Node<Type>
+  private tail: Node<Type>
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  get _head(){
+    return this.head ? this.head.value : null;
+  }
+
+  get _tail(){
+    return this.tail ? this.tail.value : null;
+  }
+}
 
