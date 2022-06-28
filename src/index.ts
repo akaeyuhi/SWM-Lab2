192ca8119fed92e48ec2becfcdbb0f9e7aa81303
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-28 22:27:58 +0300

    List getElement method

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,20 @@ interface Node<Type> {
 
 
 export default class LinkedList<Type> {
-  private head: Node<Type>
-  private tail: Node<Type>
+  private head: Node<Type>;
+  private tail: Node<Type>;
 
   constructor() {
     this.head = null;
     this.tail = null;
   }
 
-  checkIndex(index: number, max: number){
-    if(index < 0 || index > max){
-      throw new Error('Incorrect index specified');
-    }
-  }
-
-  get length(): number{
+  get length(): number {
     let temp: Node<Type> = this.head;
-    let count: number = 0;
+    let count = 0;
 
-    while(true){
-      if(temp === null || temp === this.tail){
+    while (true) {
+      if (temp === null || temp === this.tail) {
         count += (temp === null ? 0 : 1);
         break;
       }
@@ -36,27 +30,67 @@ export default class LinkedList<Type> {
     return count;
   }
 
+  get _head() {
+    return this.head ? this.head.value : null;
+  }
+
+  get _tail() {
+    return this.tail ? this.tail.value : null;
+  }
+
+  checkIndex(index: number, max: number) {
+    if (index < 0 || index > max) {
+      throw new Error('Incorrect index specified');
+    }
+  }
+
   append(data: Type) {
     const nodeToAppend: Node<Type> = {
       value: data,
       next: null,
-      prev: this.tail
+      prev: this.tail,
     };
 
-    if(this.length){
-      this.tail.next = nodeToAppend;
+    console.log(this.length);
+
+    if (this.length) {
       this.tail = nodeToAppend;
-    } else this.head = nodeToAppend;
+      if (this.length > 1) {
+        this.tail.prev.next = nodeToAppend;
+      } else {
+        this.tail.prev = this.head;
+      }
+    } else {
+      this.head = nodeToAppend;
+    }
 
-    if(this.length === 1) this.head.next = this.tail;
+    if (this.length === 1) {
+      this.head.next = this.tail;
+    }
   }
 
-  get _head(){
-    return this.head ? this.head.value : null;
-  }
+  getElement(index: number): Node<Type> {
+    this.checkIndex(index, this.length - 1);
 
-  get _tail(){
-    return this.tail ? this.tail.value : null;
+    let temp: Node<Type> = this.head;
+    for (let i = 0; i < this.length; i++) {
+      if (i === index) return temp;
+
+      temp = temp.next;
+    }
   }
 }
 
+const list = new LinkedList<string>();
+
+
+list.append('1');
+list.append('3');
+list.append('2');
+list.append('4');
+
+console.log(list.length);
+
+for (let i = 0; i < list.length; i++) {
+  console.dir(list.getElement(i), { depth: 10 });
+}
