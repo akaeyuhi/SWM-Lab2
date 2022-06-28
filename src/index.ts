efb376663ecd2be203219ab394861867291e7a2f
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-28 23:58:20 +0300

    delete unused tests from index

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,3 @@ list.append('1');
 list.append('3');
 list.append('2');
 list.append('4');
-// list.insert('inserted', 2);
-// list.deleteNode(2);
-// list.append('5');
-// list.append('5');
-// for (let i = 0; i < list.length; i++) {
-//   console.dir(list.getElement(i), { depth: 10 });
-// }
-// list.deleteAll('5');
-// for (let i = 0; i < list.length; i++) {
-//   console.dir(list.getElement(i), { depth: 10 });
-// }
-//
-// const copiedList = list.clone();
-//
-// console.dir(list.getElementsAsArray(), { depth: 10 });
-// console.dir(copiedList.getElementsAsArray(), { depth: 10 });
-
-// list.reverse();
-// console.dir(list.getElementsAsArray(), { depth: 10 });
-// list.append('1');
-// list.append('5');
-//
-// console.log(list.findFirst('1'));
-// console.log(list.findLast('1'));
-
-// list.clear();
-// console.log(list.getElementsAsArray());
-
-// const listToExtend = new LinkedList<string>();
-// listToExtend.append('extend1');
-// listToExtend.append('extend2');
-// list.extend(listToExtend);
-// console.log(list.getElementsAsArray());
